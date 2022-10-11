1ba896bd4e926b79fe4ffc8bc106b81f740cc47c
Alexander K <jonhy.blaze@gmail.com>
2022-10-11 11:50:45 +0300

    setting react app, linking assets

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header () {
+  <div className="header">
+    <img src="../assets/header-logo.png"
+         height="30px" />
+    <h4>my travel journal</h4>
+  </div>
+}
+
+export default Header
